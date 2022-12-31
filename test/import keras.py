8e36f47b73c4abed2e8f91bc0833cbb46e353180
777beer <beer777@gmail.com>
2022-12-31 18:13:49 +0900

    web_arch_labo

---
+++ test/import keras.py
@@ -0,0 +1,51 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, InputLayer
+from keras.optimizers import RMSprop
+
+
+#モデルの保存
+import os
+from google.colab import files
+
+# MNISTデータを読込む
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# MNISTデータを加工する
+x_train  = x_train.reshape(60000, 784)
+x_test   = x_test.reshape(10000, 784)
+x_train  = x_train.astype('float32')
+x_test   = x_test.astype('float32')
+x_train /= 255
+x_test  /= 255
+y_train  = keras.utils.to_categorical(y_train, 10)
+y_test   = keras.utils.to_categorical(y_test, 10)
+
+# モデルの構築
+model = Sequential()
+model.add(InputLayer(input_shape=(784,)))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+# 学習
+epochs = 20
+batch_size = 128
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+
+# 検証
+score = model.evaluate(x_test, y_test, verbose=1)
+print()
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#resultsディレクトリを作成
+result_dir = 'results'
+if not os.path.exists(result_dir):
+    os.mkdir(result_dir)
+
+# 重みを保存
+model.save(os.path.join(result_dir, 'model.h5'))
+
+files.download( '/content/results/model.h5' ) 
+
